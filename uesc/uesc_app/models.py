c0a419157d49fc32a61c84c14cd8f0808484e3fb
gsousa143 <gabrielsousa4242@gmail.com>
2024-03-22 07:08:25 -0300

    att projeto uesc

--- uesc/uesc_app/models.py
+++ uesc/uesc_app/models.py
@@ -2,8 +2,10 @@ from django.db import models
 
 class Resultado(models.Model):
     nome = models.CharField(max_length=200)
+    titulo = models.CharField(max_length=200)
     data = models.DateField(auto_now_add=True)
     link = models.CharField(max_length=200)
+    obs = models.CharField(max_length=200)
 
     def __str__(self):
         return self.nome
